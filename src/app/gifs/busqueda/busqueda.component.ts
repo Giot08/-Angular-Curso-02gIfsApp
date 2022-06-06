75b6f2802d22c0280f17a731c958436e5e3e0e4d
giot08 <xgiot08@gmail.com>
2022-06-06 13:36:01 -0400

    commit

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,15 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [],
+})
+export class BusquedaComponent {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+  buscar(termino: string) {
+    const valor = this.txtBuscar.nativeElement.value
+    console.log(valor)
+    this.txtBuscar.nativeElement.value = ''
+  }
+}
